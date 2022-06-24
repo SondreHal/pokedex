000796fc4d292e357cd9ff8802c2c2b49bc19c51
Trezlor <77504889+Trezlor@users.noreply.github.com>
2022-06-24 20:11:05 +0200

    hoi

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const pokeName = document.querySelector(".pokename");
 const pokeId = document.querySelector(".pokeid");
 const pokeSprite = document.querySelector(".pokesprite");
+const pokeType = document.querySelector(".type");
 
+//Function to fetch a pokemon's Name, Type, Sprite & ID based on a random number between 1-905
 async function Pokemon() {
-	const fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-	const pokeData = await fetchPokemon.json();
-	const pokeDataName =
-		pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1);
+	const randomNumber = Math.floor(Math.random() * 905 + 1); //Random number 1-905
+	const fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber); //Fetch PokeAPI
+	const pokeData = await fetchPokemon.json(); //Stringify PokeAPI
+
+	const findPokeName =
+		pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1); //Finds pokemon's name and makes first letter uppercase
+	const findPokeType = pokeData.types[0].type.name; //Finds pokemon's type
 
 	console.log(pokeData);
 
-	pokeName.textContent = pokeDataName;
-	pokeId.textContent = pokeData.id;
-	pokeSprite.innerHTML = "<img src=" + pokeData.sprites.front_default + ">";
+	pokeName.textContent = findPokeName; //Render pokemon's name
+	pokeType.innerHTML = "<img src='type_images/" + findPokeType + ".png'>"; //Render pokemon's type
+	pokeSprite.innerHTML = "<img src=" + pokeData.sprites.front_default + ">"; //Render pokemon's sprite
+	pokeId.textContent = pokeData.id; //Render pokemon's id
 }
 
 Pokemon();
