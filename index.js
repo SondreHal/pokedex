f5b444f32acb31396d6048460666494e2446ac35
SondreHal <sondrehal.kodehode@gmail.com>
2022-06-29 02:25:41 +0200

    hoi

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const pokeId = document.querySelector(".pokeid");
 //Function to fetch a pokemon's Name, Type, Sprite & ID based on a random number between 1-905
 async function Pokemon() {
 	const randomNumber = Math.floor(Math.random() * 905 + 1); //Random number 1-905
-	const fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber); //Fetch PokeAPI
-	const pokeData = await fetchPokemon.json(); //Stringify PokeAPI
+	const fetchPokemonData = await fetch("https://pokeapi.co/api/v2/pokemon/"); //Fetch PokeAPI
+	const pokeData = await fetchPokemonData.json(); //Stringify PokeAPI
+	console.log(pokeData.results[4].url);
+
+	const fetchPokeSpecies = await fetch(
+		"https://pokeapi.co/api/v2/pokemon-species/" + randomNumber
+	);
+	const pokeSpecies = await fetchPokeSpecies.json();
+
+	const fetchEvoChain = await fetch(pokeSpecies.evolution_chain.url);
+	const evoChain = await fetchEvoChain.json();
+	// console.log(evoChain.chain.evolves_to);
 
 	const findPokeName =
 		pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1); //Finds pokemon's name and makes first letter uppercase
@@ -38,14 +48,23 @@ async function Pokemon() {
 	}
 	changeToMainType();
 
-	pokeCardId.id = changeToMainType(); //Changes the id of the div containing the pokemon types
-	pokeName.textContent = findPokeName; //Render pokemon's name
-	pokeType.innerHTML = "<img src='main_types_images/" + changeToMainType() + ".png'>"; //Render pokemon's type
-	pokeSprite.innerHTML =
-		"<img src=" + pokeData.sprites.other["official-artwork"].front_default + ">"; //Render pokemon's sprite
-	pokeId.textContent = "#" + pokeData.id; //Render pokemon's id
+	function evolve() {
+		if (evoChain.chain.species.name === pokeData.name) {
+			return "_basic";
+		} else if (evoChain.chain.evolves_to[0].species.name === pokeData.name) {
+			return "_stage_1";
+		} else {
+			return "_stage_2";
+		}
+	}
+	evolve();
 
-	console.log(pokeData);
+	pokeCardId.id = changeToMainType() + evolve(); //Changes the id of the div containing the pokemon types
+	pokeName.textContent = findPokeName; //Render pokemon's name
+	// pokeType.innerHTML = "<img src='main_types_images/" + changeToMainType() + ".png'>"; //Render pokemon's type
+	pokeSprite.innerHTML =
+		"<img src=" + pokeData.sprites.other["official-artwork"].front_default + ">";
+	pokeId.textContent = "#" + pokeData.id; //Render pokemon's id
 
 	console.log("Sub Type: " + findSubType);
 	console.log("Main Type: " + changeToMainType());
