951dff7b06cd7b36fb2c6676924cb4b774ea8a1b
Trezlor <77504889+Trezlor@users.noreply.github.com>
2022-06-29 03:35:55 +0200

    hoi

--- index.js
+++ index.js
@@ -6,23 +6,33 @@ const pokeId = document.querySelector(".pokeid");
 
 //Function to fetch a pokemon's Name, Type, Sprite & ID based on a random number between 1-905
 async function Pokemon() {
-	const randomNumber = Math.floor(Math.random() * 905 + 1); //Random number 1-905
-	const fetchPokemonData = await fetch("https://pokeapi.co/api/v2/pokemon/"); //Fetch PokeAPI
-	const pokeData = await fetchPokemonData.json(); //Stringify PokeAPI
-	console.log(pokeData.results[4].url);
+	//Random number 1-905
+	const randomNumber = Math.floor(Math.random() * 905 + 1);
 
+	//Fetch pokemonAPI
+	const fetchPokemonData = await fetch(
+		"https://pokeapi.co/api/v2/pokemon/" + randomNumber
+	);
+	//Stringify pokemonAPI
+	const pokeData = await fetchPokemonData.json();
+
+	//Fetch pokemonSpeciesAPI
 	const fetchPokeSpecies = await fetch(
 		"https://pokeapi.co/api/v2/pokemon-species/" + randomNumber
 	);
+	//Stringify pokemonSpeciesAPI
 	const pokeSpecies = await fetchPokeSpecies.json();
 
+	//Fetch evolutionChainAPI
 	const fetchEvoChain = await fetch(pokeSpecies.evolution_chain.url);
+	//Stringify evolutionChainAPI
 	const evoChain = await fetchEvoChain.json();
-	// console.log(evoChain.chain.evolves_to);
 
+	//Finds pokemon's name and makes first letter uppercase
 	const findPokeName =
-		pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1); //Finds pokemon's name and makes first letter uppercase
-	const findSubType = pokeData.types[0].type.name; //Finds pokemon's type
+		pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1);
+	//Finds pokemon's type
+	const findSubType = pokeData.types[0].type.name;
 
 	//Function that changes the sub type of the pokemon to the main type (Trading Card Game)
 	function changeToMainType() {
@@ -48,6 +58,7 @@ async function Pokemon() {
 	}
 	changeToMainType();
 
+	//Function to figure out a pokemon's stage in an evolution if it has any, and returns it
 	function evolve() {
 		if (evoChain.chain.species.name === pokeData.name) {
 			return "_basic";
@@ -59,12 +70,20 @@ async function Pokemon() {
 	}
 	evolve();
 
-	pokeCardId.id = changeToMainType() + evolve(); //Changes the id of the div containing the pokemon types
-	pokeName.textContent = findPokeName; //Render pokemon's name
-	// pokeType.innerHTML = "<img src='main_types_images/" + changeToMainType() + ".png'>"; //Render pokemon's type
+	//Changes the id of the div containing the pokemon types
+	pokeCardId.id = changeToMainType() + evolve();
+
+	//Render pokemon's name
+	pokeName.textContent = findPokeName;
+	//Render pokemon's sprite
 	pokeSprite.innerHTML =
-		"<img src=" + pokeData.sprites.other["official-artwork"].front_default + ">";
-	pokeId.textContent = "#" + pokeData.id; //Render pokemon's id
+		"<img src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" +
+		randomNumber +
+		".png>";
+	//Render pokemon's id
+	pokeId.textContent = "#" + pokeData.id;
+
+	pokeSprite.innerHTML;
 
 	console.log("Sub Type: " + findSubType);
 	console.log("Main Type: " + changeToMainType());
